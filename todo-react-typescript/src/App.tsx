e95bdbd1a99e8d49a494ec60c7a70f4795c2745e
Saji <sewangco@gmail.com>
2020-12-11 13:34:48 +0100

    todoListCreated

---
+++ todo-react-typescript/src/App.tsx
@@ -0,0 +1,47 @@
+
+import React, { useState } from 'react'
+import { AddTodoForm } from './Components/AddTodoForm';
+import { TodoList } from './Components/TodoList';
+
+
+
+const initialTodos: Array<Todo> = [
+    {text: "walk the dog", complete: true},
+    {text:"write app", complete: false}
+];
+
+const App: React.FC = () => {
+    const [todos, setTodos]= useState(initialTodos)
+    
+    const toggleTodo: ToggleTodo= selectedTodo => {
+        const newTodos = todos.map(todo => {
+            if (todo === selectedTodo){
+                return {
+                    ...todo,
+                    complete: !todo.complete
+                }
+            }
+            return todo;
+        });
+        setTodos(newTodos);
+    };
+
+    const addTodo: AddTodo= newTodo => {
+        newTodo.trim()!== "" &&
+            setTodos([...todos,{text: newTodo, complete: false}]);
+        
+    };
+
+    return (
+        
+        <React.Fragment>
+          <h1>What's the plan for today?</h1>
+          <TodoList todos={todos} toggleTodo={toggleTodo} />
+          <AddTodoForm addTodo={addTodo}/>
+
+        </React.Fragment>
+    );
+  }
+  
+  export default App;
+  
---
+++ todo-react-typescript/src/Components/AddTodoForm.tsx
@@ -0,0 +1,31 @@
+import React, {ChangeEvent, FormEvent, useState} from 'react'
+
+
+interface AddTodoFormProps {
+    addTodo: AddTodo;
+}
+
+export const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
+    const [newTodo, setNewTodo] = useState("")
+
+    const handleChange= (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value);
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLButtonElement>)=>{
+        e.preventDefault();
+        addTodo(newTodo);
+        setNewTodo("");
+    };
+
+    return ( 
+    <React.Fragment>
+    <form>
+        <input type="text" value={newTodo} onChange={handleChange} />
+        <button type="submit" onClick={handleSubmit}>
+            Add Todo
+        </button>
+    </form>
+    </React.Fragment>
+ )
+}
